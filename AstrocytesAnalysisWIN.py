7ca72a1b494676010cf4dd3233f280b327c90660
GeneChang1 <genechang0219@gmail.com>
2023-07-14 10:33:29 -0700

    tweaking windows and mac file to work

--- AstrocytesAnalysisWIN.py
+++ AstrocytesAnalysisWIN.py
@@ -1,10 +1,8 @@
 import numpy as np
 import multiprocessing
-import utils
-
 from cellpose import plot, utils
 from matplotlib import pyplot as plt
-dat = np.load('/Users/genechang/Downloads/TrainingData01/0_seg.npy', allow_pickle=True).item()
+
 
 
 
@@ -13,23 +11,58 @@ if __name__ == '__main__':
     multiprocessing.freeze_support()
 
     # plot image with masks overlaid
+    def clamp(num, min_value, max_value):
+        return max(min(num, max_value), min_value)
+
+    dat = np.load('/Users/genechang/Downloads/TrainingData01/0_seg.npy', allow_pickle=True).item()
+
+    # plot image with masks overlaid
     mask_RGB = plot.mask_overlay(dat['img'], dat['masks'],
                             colors=np.array(dat['colors']))
 
     # plot image with outlines overlaid in red
     outlines = utils.outlines_list(dat['masks'])
-    plt.imshow(dat['img'])
+
+    samplingImage = plt.imread('/Users/genechang/Downloads/TrainingData01/Cell39_Pre.tiff')
+    plt.imshow(samplingImage)
+
+    #fig = plt.figure()#fig, axs = plt.subplots(len(outlines))
+
     for o in outlines:
-        CenterX = 0
-        CenterY = 0
+        #get average x and y
+        centerX = 0
+        centerY = 0
+        minX = o[:,0][0]
+        maxX = o[:,0][0]
+
         plt.plot(o[:,0], o[:,1], color='r')
+
         for x in o[:,0]:
-            CenterX += x
+            centerX+=x
+            if x < minX:
+                minX = x
+            if x > maxX:
+                maxX = x
         for y in o[:,1]:
-            CenterY += y 
-        
+            centerY += y
+        centerX /= len(o[:,0])
+        centerY /= len(o[:,1])
+
+        #get standard deviation
+        stdX = np.std(o[:,0])
+        stdY = np.std(o[:,1])
+        stdMax = max(stdX, stdY)
+
+        plt.plot(centerX, centerY, color='r', marker=".")
+        plt.plot(centerX, centerY, marker=".", markerfacecolor=(0, 0, 0, 0), markeredgecolor=(0, 0, 1, 1), markersize=stdMax)
+
+        #plt.add_patch(patches.Circle((centerX, centerY),radius=stdMax))
+
+        #plt.fill_between([minX, maxX], [o[:,0], o[:,1]], )
+
     plt.show()
 
     if __name__ == '__main__':
         multiprocessing.freeze_support()
+
 
