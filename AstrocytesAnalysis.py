77dcffb3b4d8b5b124251e039b609e1807598096
not-availiable <arthongreenspan@gmail.com>
2023-07-18 14:30:41 -0700

    windows bugfix and reformatted code to be more modular

--- AstrocytesAnalysis.py
+++ AstrocytesAnalysis.py
@@ -4,64 +4,35 @@ import numpy as np
 import numpy.ma as ma
 from cellpose import plot, utils
 from matplotlib import pyplot as plt
+import multiprocessing
 
-def create_circular_mask(h, w, center=None, radius=None):
-    if center is None: # use the middle of the image
-        center = (int(w/2), int(h/2))
-    if radius is None: # use the smallest distance between the center and image walls
-        radius = min(center[0], center[1], w-center[0], h-center[1])
-
-    Y, X = np.ogrid[:h, :w]
-    dist_from_center = np.sqrt((X - center[0])**2 + (Y-center[1])**2)
-
-    mask = dist_from_center <= radius
-    return mask
-
-if __name__ == '__main__':
-    f = open("nucleiMaskLocation.txt")
-    nucDat = np.load(f.readline().rstrip(), allow_pickle=True).item()
+def load_image_path(file):
+    f = open(file)
+    path = f.readline().rstrip()
     f.close()
+    return path
 
-    f = open("cytoMaskLocation.txt")
-    cytoDat = np.load(f.readline().rstrip(), allow_pickle=True).item()
-    f.close()
+def get_center_location(o):
+    centerX = 0
+    centerY = 0
 
-    # plot image with outlines overlaid in red
-    nucOutlines = utils.outlines_list(nucDat['masks'])
-    cytoOutlines = utils.outlines_list(cytoDat['masks'])
+    for x in o[:,0]:
+        centerX+=x
+    for y in o[:,1]:
+        centerY += y
+    centerX /= len(o[:,0])
+    centerY /= len(o[:,1])
 
-    f = open("imgLocation.txt")
-    samplingImage = plt.imread(f.readline().rstrip())
-    f.close()
+    return centerX, centerY
 
-    masks = []
-    nucWholeMask = nucDat['masks']
-    nucWholeMask = nucWholeMask > 0
-
-    cytoWholeMask = cytoDat['masks']
-
-    # i = 0
+def generate_masks():
     for o in nucOutlines:
         # nuclei
         # get average x and y
-        centerX = 0
-        centerY = 0
-        minX = o[:,0][0]
-        maxX = o[:,0][0]
+        centerX, centerY = get_center_location(o)
 
         plt.plot(o[:,0], o[:,1], color='r')
         # plt.plot(c[:,0], c[:,1], color='r')
-
-        for x in o[:,0]:
-            centerX+=x
-            if x < minX:
-                minX = x
-            if x > maxX:
-                maxX = x
-        for y in o[:,1]:
-            centerY += y
-        centerX /= len(o[:,0])
-        centerY /= len(o[:,1])
 
         #get standard deviation
         stdX = np.std(o[:,0])
@@ -75,14 +46,7 @@ if __name__ == '__main__':
         hasCloseCytoplasm = False
         closeMaskId = 1
         for c in cytoOutlines: 
-            cytoCenterX = 0
-            cytoCenterY = 0
-            for x in c[:,0]:
-                cytoCenterX+=x
-            for y in c[:,1]:
-                cytoCenterY += y
-            cytoCenterX /= len(c[:,0])
-            cytoCenterY /= len(c[:,1])
+            cytoCenterX, cytoCenterY = get_center_location(c)
             if math.dist([centerX, centerY], [cytoCenterX, cytoCenterY]) < 50:
                 hasCloseCytoplasm = True
                 break
@@ -99,9 +63,8 @@ if __name__ == '__main__':
         # remove the nucleus from the mask
         mask[nucWholeMask] = 0
         masks.append(mask)
-        # i+=1
 
-    # stitch together all of the masks
+def displayData():
     fullMask = np.zeros(nucWholeMask.shape)
     for mask in masks:
         fullMask = np.add(fullMask, mask)
@@ -111,5 +74,37 @@ if __name__ == '__main__':
     plt.imshow(samplingImage)
     plt.show()
 
+def create_circular_mask(h, w, center=None, radius=None):
+    if center is None: # use the middle of the image
+        center = (int(w/2), int(h/2))
+    if radius is None: # use the smallest distance between the center and image walls
+        radius = min(center[0], center[1], w-center[0], h-center[1])
+
+    Y, X = np.ogrid[:h, :w]
+    dist_from_center = np.sqrt((X - center[0])**2 + (Y-center[1])**2)
+
+    mask = dist_from_center <= radius
+    return mask
+
 if __name__ == '__main__':
     multiprocessing.freeze_support()
+
+    nucDat = np.load(load_image_path("nucleiMaskLocation.txt"), allow_pickle=True).item()
+    cytoDat = np.load(load_image_path("cytoMaskLocation.txt"), allow_pickle=True).item()
+
+    # plot image with outlines overlaid in red
+    nucOutlines = utils.outlines_list(nucDat['masks'])
+    cytoOutlines = utils.outlines_list(cytoDat['masks'])
+
+    samplingImage = plt.imread(load_image_path("imgLocation.txt"))
+
+    masks = []
+    nucWholeMask = nucDat['masks']
+    nucWholeMask = nucWholeMask > 0
+
+    cytoWholeMask = cytoDat['masks']
+
+    generate_masks()
+
+    # stitch together all of the masks
+    displayData()
