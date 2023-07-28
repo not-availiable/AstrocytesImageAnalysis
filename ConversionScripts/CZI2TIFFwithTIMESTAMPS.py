62ca35f14c338c3957f71d6d464df12a9262980b
Aether9867 <137755686+Aether9867@users.noreply.github.com>
2023-07-28 00:33:49 -0700

    Fast conversions + color fix
    much much faster conversion and renaming compared to old code and made it isolate to green channel like it was supposed to

--- ConversionScripts/CZI2TIFFwithTIMESTAMPS.py
+++ ConversionScripts/CZI2TIFFwithTIMESTAMPS.py
@@ -1,30 +1,41 @@
 from czifile import CziFile
 import xml.etree.ElementTree as ET
 import os
-import tifffile
+import cv2
+import tifffile as tf
 import time
-start_time=time.time()
+from multiprocessing import Pool
+
 def extract_timestamps_from_metadata(metadata_xml):
     root = ET.fromstring(metadata_xml)
     timestamps = [element.text for element in root.iter('Time')]
     return timestamps
 
+def save_slice_as_tiff(args):
+    slice_img, timestamp, i, output_dir = args
+    tiff_file_name = f"{timestamp}_{i}.tiff"
+    tf.imwrite(os.path.join(output_dir, tiff_file_name), slice_img)
+    src = cv2.imread(os.path.join(output_dir, tiff_file_name))
+    src[:,:,0] = 0
+    src[:,:,2] = 0
+    cv2.imwrite(os.path.join(output_dir, tiff_file_name), src)
+
 def convert_czi_to_tiff(czi_file_path, output_dir):
     with CziFile(czi_file_path) as czi:
         img = czi.asarray()
         metadata_xml = czi.metadata()
-        
+
         timestamps = extract_timestamps_from_metadata(metadata_xml)
 
-        for i, slice_img in enumerate(img):
-            timestamp = timestamps[i].replace(":", "_").replace(".", "_")
-            tiff_file_name = f"{timestamp}_{i}.tiff"
-            tifffile.imwrite(os.path.join(output_dir, tiff_file_name), slice_img)
+        with Pool() as pool:
+            pool.map(save_slice_as_tiff, [(slice_img, timestamps[i].replace(":", "_").replace(".", "_"), i, output_dir) for i, slice_img in enumerate(img)])
 
-czi_file = "CZI_FILE_HERE"
-output_dir = "OUTPUT_DIRECTORY_HERE"
+if __name__ == "__main__":
+    czi_file = "/media/krishna/OS/OPALSdata/czi2tiff/czi/Cell_91_Pre.czi"
+    output_dir = "/media/krishna/OS/OPALSdata/czi2tiff/output"
 
-convert_czi_to_tiff(czi_file, output_dir)
-end_time=time.time()
-final=end_time-start_time
-print(final)
+    start_time = time.time()
+    convert_czi_to_tiff(czi_file, output_dir)
+    end_time = time.time()
+    total_time = end_time - start_time
+    print("Total time taken: {:.2f} seconds".format(total_time))
