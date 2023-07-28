159872ca430663bf954488f8f0e0adba3ca73ace
Aether9867 <137755686+Aether9867@users.noreply.github.com>
2023-07-28 11:03:49 -0700

    Faster conversion
    even faster conversion from czi to timestamped tiif with mutithreading (faster I/O operations) and dask implementation

--- ConversionScripts/CZI2TIFFwithTIMESTAMPS.py
+++ ConversionScripts/CZI2TIFFwithTIMESTAMPS.py
@@ -1,34 +1,45 @@
 from czifile import CziFile
 import xml.etree.ElementTree as ET
 import os
-import cv2
-import tifffile as tf
+import tifffile
 import time
-from multiprocessing import Pool
+import cv2
+import dask
 
 def extract_timestamps_from_metadata(metadata_xml):
     root = ET.fromstring(metadata_xml)
     timestamps = [element.text for element in root.iter('Time')]
     return timestamps
 
-def save_slice_as_tiff(args):
-    slice_img, timestamp, i, output_dir = args
+@dask.delayed
+def worker(slice_img, timestamp, i, output_dir):
     tiff_file_name = f"{timestamp}_{i}.tiff"
-    tf.imwrite(os.path.join(output_dir, tiff_file_name), slice_img)
-    src = cv2.imread(os.path.join(output_dir, tiff_file_name))
-    src[:,:,0] = 0
-    src[:,:,2] = 0
-    cv2.imwrite(os.path.join(output_dir, tiff_file_name), src)
+    tifffile_path = os.path.join(output_dir, tiff_file_name)
+    tifffile.imwrite(tifffile_path, slice_img)
+
+    # Read the image back in
+    img = cv2.imread(tifffile_path)
+
+    # Set the red and blue channels to 0
+    img[:,:,0] = 0
+    img[:,:,2] = 0
+
+    # Save the modified image back to the file
+    cv2.imwrite(tifffile_path, img)
 
 def convert_czi_to_tiff(czi_file_path, output_dir):
     with CziFile(czi_file_path) as czi:
         img = czi.asarray()
         metadata_xml = czi.metadata()
-
         timestamps = extract_timestamps_from_metadata(metadata_xml)
 
-        with Pool() as pool:
-            pool.map(save_slice_as_tiff, [(slice_img, timestamps[i].replace(":", "_").replace(".", "_"), i, output_dir) for i, slice_img in enumerate(img)])
+        tasks = []
+        for i, slice_img in enumerate(img):
+            timestamp = timestamps[i].replace(":", "_").replace(".", "_")
+            tasks.append(worker(slice_img, timestamp, i, output_dir))
+
+        # Execute all tasks
+        dask.compute(*tasks)
 
 if __name__ == "__main__":
     czi_file = "/media/krishna/OS/OPALSdata/czi2tiff/czi/Cell_91_Pre.czi"
